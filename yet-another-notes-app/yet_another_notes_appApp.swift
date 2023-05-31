932073069679c2728d9e899cbc4902bde0eb5bb8
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2023-05-31 15:19:47 +0200

    Initial Commit

---
+++ yet-another-notes-app/yet_another_notes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  yet_another_notes_appApp.swift
+//  yet-another-notes-app
+//
+//  Created by Adam Cvikl on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct yet_another_notes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
